f01a911d511f942a94ec99a9192f7aefd69693f1
pnzenang <pnzenang@gmail.com>
2026-02-20 03:06:12 -0500

    2.20.commit

--- app/(pages)/deceased-members/page.tsx
+++ app/(pages)/deceased-members/page.tsx
@@ -1,7 +1,37 @@
-import { fetchProfile, allMembersAction } from '@/utils/actions';
+import { columns } from '@/components/data-table/columns';
+import { DataTable } from '@/components/data-table/data-table';
+import { Shell } from '@/components/shells/shell';
+import { taskSchema } from '@/lib/validations/schema';
+import { z } from 'zod';
 
-const DeceasedMembers = async () => {
-  const profile = await fetchProfile();
-  return <h1 className='text-3xl py-32'>Deceased Members</h1>;
-};
-export default DeceasedMembers;
+export const runtime = 'edge';
+
+async function getTasks() {
+  const res = await fetch(
+    'https://my.api.mockaroo.com/tasks.json?key=f0933e60',
+  );
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  const data = await res.json();
+
+  // ** Workaround as my mock api has date returned as "dd-Mon-yyyy"
+  const tasks = z.array(taskSchema).parse(
+    data.map((task: any) => {
+      task.due_date = new Date(Date.parse(task.due_date));
+      return task;
+    }),
+  );
+  return tasks;
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks();
+  return (
+    <Shell>
+      <div className='flex h-full min-h-screen w-full flex-col'>
+        <DataTable data={tasks} columns={columns} />
+      </div>
+    </Shell>
+  );
+}
