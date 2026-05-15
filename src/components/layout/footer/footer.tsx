7ea1b6bcb1c5a969f98a38490ac2a0f3e8f13df2
pnzenang <pnzenang@gmail.com>
2026-05-15 02:37:45 -0400

    5.15.commit

--- src/components/layout/footer/footer.tsx
+++ src/components/layout/footer/footer.tsx
@@ -8,7 +8,7 @@ import Logo from '@/components/logo'
 const Footer = () => {
   return (
     <footer className='relative overflow-hidden'>
-      <div className='pb mt-10 sm:px-16 md:-mb-22 lg:px-24'>
+      <div className='mt-10 pb-10 sm:px-16 md:-mb-22 lg:px-24'>
         <HoverText text='SAGI' />
       </div>
     </footer>
